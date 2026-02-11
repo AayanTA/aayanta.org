673053e9c74112ca07dd1af21039a4a855b3096a
aayanta <aayanta@outlook.com>
2026-02-11 15:17:21 +1100

    Refactor Player class: remove unused id parameter, streamline particle management, and enhance thrust particle rendering

--- projects/spaceshooter/player.js
+++ projects/spaceshooter/player.js
@@ -1,98 +1,91 @@
+import { WIDTH, HEIGHT } from "./constants.js";
+
 export class Player {
-  constructor(x, y, color, controls, id) {
+  constructor(x, y, color, controls) {
     this.x = x;
     this.y = y;
-    this.color = color;
-    this.controls = controls;
-    this.id = id;
 
     this.vx = 0;
     this.vy = 0;
+
     this.angle = 0;
     this.radius = 12;
+
+    this.color = color;
+    this.controls = controls;
 
     this.laps = 0;
     this.checkpointIndex = 0;
 
     this.stun = 0;
     this.fireCooldown = 0;
+
+    this.particles = [];   // ✅ FIXED
   }
 
   update(keys) {
-    if (this.portalCooldown > 0) this.portalCooldown--;
 
     if (this.stun > 0) {
       this.stun--;
       return;
     }
 
-    const TURN = 0.07;
-    const THRUST = 0.22;
-    const FRICTION = 0.985;
+    // Rotation
+    if (keys[this.controls.left]) this.angle -= 0.06;
+    if (keys[this.controls.right]) this.angle += 0.06;
 
-    if (keys[this.controls.left]) this.angle -= TURN;
-    if (keys[this.controls.right]) this.angle += TURN;
-
+    // Thrust
     if (keys[this.controls.thrust]) {
-      this.vx += Math.cos(this.angle) * THRUST;
-      this.vy += Math.sin(this.angle) * THRUST;
+      this.vx += Math.cos(this.angle) * 0.25;
+      this.vy += Math.sin(this.angle) * 0.25;
 
-      this.spawnParticle();
+      // Add thrust particles
+      this.particles.push({
+        x: this.x - Math.cos(this.angle) * 14,
+        y: this.y - Math.sin(this.angle) * 14,
+        life: 20
+      });
     }
 
-    this.vx *= FRICTION;
-    this.vy *= FRICTION;
+    // Friction
+    this.vx *= 0.99;
+    this.vy *= 0.99;
 
     this.x += this.vx;
     this.y += this.vy;
 
-    this.prevX = this.x;
-    this.prevY = this.y;
-
-
-    this.updateParticles();
-  }
-
-  spawnParticle() {
-    this.particles.push({
-      x: this.x - Math.cos(this.angle) * 14,
-      y: this.y - Math.sin(this.angle) * 14,
-      vx: -Math.cos(this.angle) * 1.5 + (Math.random() - 0.5),
-      vy: -Math.sin(this.angle) * 1.5 + (Math.random() - 0.5),
-      life: 20
-    });
-  }
-
-  updateParticles() {
-    this.particles.forEach(p => {
-      p.x += p.vx;
-      p.y += p.vy;
-      p.life--;
-    });
-
+    // Keep particles updated
+    this.particles.forEach(p => p.life--);
     this.particles = this.particles.filter(p => p.life > 0);
   }
 
   draw(ctx) {
+
+    // Draw particles
+    ctx.fillStyle = this.color;
     this.particles.forEach(p => {
       ctx.globalAlpha = p.life / 20;
-      ctx.fillStyle = "orange";
-      ctx.fillRect(p.x, p.y, 2, 2);
+      ctx.beginPath();
+      ctx.arc(p.x, p.y, 3, 0, Math.PI * 2);
+      ctx.fill();
       ctx.globalAlpha = 1;
     });
 
+    // Draw ship
     ctx.save();
     ctx.translate(this.x, this.y);
     ctx.rotate(this.angle);
 
-    ctx.fillStyle = this.color;
+    ctx.strokeStyle = this.color;
+    ctx.lineWidth = 2;
+
     ctx.beginPath();
-    ctx.moveTo(16, 0);
-    ctx.lineTo(-10, 8);
-    ctx.lineTo(-6, 0);
+    ctx.moveTo(14, 0);
     ctx.lineTo(-10, -8);
+    ctx.lineTo(-6, 0);
+    ctx.lineTo(-10, 8);
     ctx.closePath();
-    ctx.fill();
+    ctx.stroke();
 
     ctx.restore();
   }
